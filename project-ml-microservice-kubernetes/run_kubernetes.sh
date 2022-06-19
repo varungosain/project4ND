5997334e0530c0ca051417c999a956d41bd97492
EC2 Default User <ec2-user@ip-172-31-82-213.ec2.internal>
2022-06-19 03:13:07 +0000

    Adding all new and Chnaged files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+alias kubectl="minikube kubectl --"
+dockerpath=varungosain/proj4vg
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl -- run proj4vg --port=80 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl -- get pods
 
 # Step 4:
 # Forward the container port to a host
-
+minikube kubectl -- port-forward proj4vg 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=varungosain/proj4vg
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u varungosain
+docker tag proj4vg $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
